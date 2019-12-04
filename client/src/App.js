480ecfccc768beed8b261987b3fc4d86541f2a13
neerajp99 <neerajp1999@gmail.com>
2019-12-04 12:38:59 +0530

    Modify App

--- client/src/App.js
+++ client/src/App.js
@@ -13,13 +13,13 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/" render={() => <Landing />} />
+        <Route exact path={"/flightlist/:v1,:v2,:v3, :v4, :v5"} component={FlightList} />
+        <Route exact path="/hotellist/:a1,:a2,:a3" component={HotelsList} />
         <Route
           exact
-          path={"/flightlist/:v1,:v2,:v3"}
-          component={FlightList}
+          path="/hotel/:a1,:a2,:a3, :a4, :a5, :a6, :a7, :a8, :a9, :a10, :a11, :a12, :a13, :a14, :a15, :a16"
+          component={Hotel}
         />
-        <Route exact path="/hotellist/:a1,:a2,:a3" component={HotelsList} />
-        <Route exact path="/hotel/:a1,:a2,:a3, :a4" component={Hotel} />
         <Route exact path="/flight" render={() => <Flight />} />
       </Router>
     </div>
